d16d08a61913656a1077814bc0a95b02a09035ad
panzz-ui <santacancut@gmail.com>
2025-09-21 12:52:16 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+function sapa() {
+    alert("Halo! kalauu suatu hari, entah kapan, kamu sadar bahwa aku benar benar sudah jadi masalalu aku hanya ingin menyampaikan satu hal, tolong jangan lupa untuk hidup dengan baik ya. bukan demi aku, tapi demi dirimu sendiri. kamu banyak hal di depan sana yang layak di perjuangkan lebih dari semua luka yang pernah kita lewati!");
+}
